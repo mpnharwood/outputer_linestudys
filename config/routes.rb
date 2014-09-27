55df6bdabdf3099d1d01b027abb4713eddfc1b1f
Matt Harwood <mpn.harwood@googlemail.com>
2014-09-27 16:55:25 +0100

    Add static-pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 OutputerLinestudyApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/about"
+  get "static_pages/help"
+  get "static_pages/contact"
+  
   resources :linestudies
 
   resources :users
