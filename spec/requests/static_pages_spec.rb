3c4fd08a637bb8021739b0fd18ccf7d0b6200de2
Matt Harwood <mpn.harwood@googlemail.com>
2014-09-27 23:39:14 +0100

    set up static pages and templates

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+	describe "Home page" do
+		it "should have the content 'Linestudy App'" do
+			visit '/static_pages/home'
+			expect(page).to have_content('Linestudy App')
+		end
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Linestudy | Home")
+		end
+	end
+
+	describe "Help page" do
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
+		end
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Linestudy | Help")
+		end
+	end
+
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("Linestudy | Contact")
+		end
+	end
+
+	describe "About page" do
+		it "should have the content 'About'" do
+			visit '/static_pages/about'
+			expect(page).to have_content('About')
+		end
+		it "should have the title 'About'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Linestudy | About")
+		end
+	end
+end
