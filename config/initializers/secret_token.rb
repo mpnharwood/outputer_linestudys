345ce527fd4612f0726f01f3d79464cf60aa5ef8
Matt Harwood <mpn.harwood@googlemail.com>
2014-09-27 13:02:49 +0100

    First commit - create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OutputerLinestudyApp::Application.config.secret_key_base = '4fef47a8b70df1bec30f2ee26f4d3eeae8c163dd3ced8a9f215499a090ef2423813d47ac24a341de9b58f306ed0b97cb0d80ccd16662a7c55c136b467a911348'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OutputerLinestudyApp::Application.config.session_store :cookie_store, key: '_outputer_linestudy_app_session'
